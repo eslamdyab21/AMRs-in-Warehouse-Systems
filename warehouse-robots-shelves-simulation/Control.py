869c6950f771f48acea398b931825de3248ff21a
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-21 03:18:41 +0200

    implement min_cost_robots function in Control class - fixes #3

--- warehouse-robots-shelves-simulation/Control.py
+++ warehouse-robots-shelves-simulation/Control.py
@@ -16,3 +16,31 @@ class Control():
         self.map_size = map_size
         self.min_cost = None
         self.robots_with_min_cost_list = []
+
+
+    def min_cost_robots(self):
+        """
+        min_cost_robots function gets robots with minimum cost to shelves, for each shelf
+        we iterate over all robots that are not paired with other shelves and compute the
+        corresponding cost until the robot with minimum cost is found, this robot is 
+        then appended to a list.
+        """
+        for shelf in self.shelvs:
+            if shelf.active_order_status:   
+                for robot in self.robots:
+                    if robot.paired_with_shelf_status == False:
+                        robot.active_order_status = False  
+                        cost_vector = abs(robot.location - shelf.location)
+                        cost = cost_vector[0] + cost_vector[1]
+                        if cost < self.min_cost:
+                            self.min_cost = cost
+                            self.robot_with_min_cost = robot
+
+                # this condition is checked for each shelf
+                if self.robot_with_min_cost != None:
+                    if self.robot_with_min_cost.active_order_status == False:
+                        self.robot_with_min_cost.active_order_status = True
+                        self.robot_with_min_cost.paired_with_shelf_status = True
+                        self.robot_with_min_cost.paired_with_shelf = shlef
+
+                        self.robots_with_min_cost_list.append(robot_with_min_cost)
