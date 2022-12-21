027a1078071949cf51472f2c07fcf89987884dea
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-21 02:39:11 +0200

    implement Shelf class and tested it - fixes #3

--- warehouse_robots_shelves_simulation.py
+++ warehouse_robots_shelves_simulation.py
@@ -68,8 +68,6 @@ class Robot():
     :param intial_orientation: (string) robot's intial_orientation (up, down, right, left)
     :param speed: (float) robot speed
     :param map_size: (list) [map_size_x, map_size_y]
-
-    :param 
     """
     def __init__(self, id, intial_location, intial_orientation, speed, map_size):
         
@@ -154,6 +152,55 @@ class Robot():
 
 
 
+class Shelf():
+    """
+    Shelf class creates Shelves for the warehouse.
+
+    :param id: (string) id for the shelf to distinguish between shelves 
+    :param intial_location: (list) [x, y]
+    :param map_size: (list) [map_size_x, map_size_y]
+    """
+    def __init__(self, id, intial_location, map_size):
+        
+        self.id = id
+        self.active_order_status = False 
+        self.paired_with_robot_status = False
+        self.paired_with_robot_id = None
+        self.intial_location = intial_location
+        self.map_size = map_size
+
+        self.prev_location = intial_location
+        self.current_location = intial_location
+        self.locations = [self.prev_location, self.current_location]
+
+
+
+    def move_forward(self):
+        """
+        move_forward function moves the shelf in the forward direction based on its 
+        current location and orientation         
+        """
+        self.prev_location = self.current_location.copy()
+
+        if (self.orientation == 'down') and (self.current_location[0] != self.map_size[1]):
+            self.current_location[0] = self.current_location[0] + 1
+            
+        elif (self.orientation == 'up') and (self.current_location[0] != 0):
+            self.current_location[0] = self.current_location[0] - 1
+
+        elif (self.orientation == 'right') and (self.current_location[1] != self.map_size[1]):
+            self.current_location[1] = self.current_location[1] + 1
+        
+        elif (self.orientation == 'left') and (self.current_location[1] != 0):
+            self.current_location[1] = self.current_location[1] - 1
+        
+
+        self.locations = [self.prev_location, self.current_location]
+
+
+
+
+
 # test the warehouse Robot and Shelf classes
 if __name__ == "__main__":
     map_size_x = 15
@@ -162,8 +209,15 @@ if __name__ == "__main__":
     map = Map2D(size_x=map_size_x, size_y=map_size_y)
     map.show_map()
 
-    R1 = Robot(id = 'R1', intial_location = [2,2], intial_orientation = 'down', speed=1, map_size = [map_size_x, map_size_y])
-    R2 = Robot(id = 'R2', intial_location = [4,3], intial_orientation = 'down', speed=1, map_size = [map_size_x, map_size_y])
+    S1 = Shelf(id = 'S1', intial_location = [5,4], map_size = [map_size_x, map_size_y])
+    S2 = Shelf(id = 'S2', intial_location = [2,2], map_size = [map_size_x, map_size_y])
+
+    map.update_objects_locations({S1.id:S1.locations, S2.id:S2.locations})
+    map.show_map()
+
+
+    R1 = Robot(id = 'R1', intial_location = [3,3], intial_orientation = 'right', speed=1, map_size = [map_size_x, map_size_y])
+    R2 = Robot(id = 'R2', intial_location = [0,3], intial_orientation = 'left', speed=1, map_size = [map_size_x, map_size_y])
     
     map.update_objects_locations({R1.id:R1.locations, R2.id:R2.locations})
     map.show_map()
