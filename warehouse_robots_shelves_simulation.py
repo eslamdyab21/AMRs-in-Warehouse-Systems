90a8f7d9785a1e33bb716d2250ec2a4dcb8aa028
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-21 02:47:43 +0200

    create Control class header - fixes #3

--- warehouse_robots_shelves_simulation.py
+++ warehouse_robots_shelves_simulation.py
@@ -200,6 +200,27 @@ class Shelf():
 
 
 
+class Control():
+    """
+    Control class controls the robots movement, find the cost of each robot path to the shelf
+    and decides which robot will move (lowest cost) and gives the steps needed to
+    get to the shelf to the chosen robot.
+
+    :param robots: (list) list of robots objects in the warehouse [robot1, robot2,....., robotn]
+    :param shelves: (list) list of shelves objects in the warehouse [shelf1, shelf2,....., shelfn]
+    :param map_size: (list) [map_size_x, map_size_y]
+    :param map_size: (list) [map_size_x, map_size_y]
+    """
+    def __init__(self, robots, shelvs, map_size):
+
+        self.robots = robots
+        self.shelvs = shelvs
+        self.map_size = map_size
+        self.min_cost = None
+        self.robots_with_min_cost_list = []
+
+
+
 
 # test the warehouse Robot and Shelf classes
 if __name__ == "__main__":
