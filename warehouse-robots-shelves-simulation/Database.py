bd397b207cccd0a6829eb0548ddc37d2910e575b
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-01-31 21:23:19 +0200

    created write_to_db & update_db functions to integrate mysql database <AMR_Warehouse> with the system - fixes #3

--- warehouse-robots-shelves-simulation/Database.py
+++ warehouse-robots-shelves-simulation/Database.py
@@ -1,3 +1,5 @@
+import mysql.connector as connector
+
 class Database():
     """
     Write and query updated data from the database
@@ -7,11 +9,9 @@ class Database():
     :param map_size: (list) [map_size_x, map_size_y]
     """
 
-    def __init__(self, robots, shelvs, map_size):
-        self.robots = robots
-        self.shelvs = shelvs
-        self.map_size = map_size
-        self.min_cost = map_size[0]
+    def __init__(self):
+        #self.min_cost = map_size[0]
+        self.connect_to_db()
 
 
     def query_from_db(self):
@@ -19,6 +19,69 @@ class Database():
         self.shelvs[1].recived_order_status = True
 
 
+    def connect_to_db(self):
+        self.connection = connector.connect(
+            user="mennatallah",
+            password="Mmeenna71@SQL",
+            port=3306,
+            host="localhost",
+            database="amr_warehouse"
+        )
+        self.cursor = self.connection.cursor()
+        print("Connection is done")
+        self.cursor.execute("""USE amr_warehouse""")
+        print("Database is in use")
 
-    def write_to_db(self):	
-        pass
+    def write_to_db(self, id, object):
+        if id[0] == 'R':
+            robot = object
+            robot_parameters = (id, robot.speed, robot.prev_location[0], robot.prev_location[1], robot.prev_location[0], robot.prev_location[1], 'None', 0, robot.battery_precentage)
+            write_to_robots = (
+                """
+                    INSERT INTO Robots(RobotID, Speed, CurrentLocationX, CurrentLocationY, NextLocationX, NextLocationY, ShelfID, CostToShelf)
+                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+                """
+            )
+            # The execution
+            self.cursor.execute(write_to_robots, robot_parameters)
+            print("A robot is added")
+
+            battery = (id, robot.battery_precentage)
+            write_to_robot_health = (
+                """
+                    INSERT INTO RobotHealth(RobotID, BatteryLife)
+                    VALUES(%s, %s)
+                """
+            )
+            self.cursor.execute(write_to_robot_health, battery)
+            print("A robot's health is added")
+
+        else:
+            shelf = object
+            shelf_parameters = (id, shelf.prev_location[0], shelf.prev_location[1], 'P2')
+            # The query we'll execute
+            write_to_shelves = (
+                """
+                    INSERT INTO Shelves(ShelfID, LocationX, LocationY, ProductID)
+                    VALUES(%s, %s, %s, %s)
+                """
+            )
+            # The execution
+            self.cursor.execute(write_to_shelves, shelf_parameters)
+
+    def update_db(self, table, id, parameters):
+        if table == 'Robots' or table == "RobotHealth":
+            primary_key = "RobotID"
+        else:
+            primary_key = "ShelfID"
+
+        for column, value in parameters.items():
+            update_robots = (
+                """
+                    UPDATE {}
+                    SET {} = {}
+                    WHERE {} = {}
+                """
+            ).format(table, column, value, primary_key, id)
+            self.cursor.execute(update_robots)
+        self.connection.commit()
--- warehouse-robots-shelves-simulation/main.py
+++ warehouse-robots-shelves-simulation/main.py
@@ -11,17 +11,38 @@ map_size_y = 8
 map = Map2D(size_x=map_size_x, size_y=map_size_y)
 # map.show_map()
 
-S1 = Shelf(id = 'S1', intial_location = [5,4], map_size = [map_size_x, map_size_y])
+# Note 1 --> Once a shelf object is created, it must be added to the database
+
+# Creating a shelf and inserting it into the database
+database = Database()
+S1 = Shelf(id = "'S1'", intial_location = [10,11], map_size = [map_size_x, map_size_y])
+database.update_db(table="Shelves", id=S1.id, parameters={"LocationX":S1.intial_location[0], "LocationY":S1.intial_location[1], "ProductID":"'P1'"})
+
+
+# Another shelf
+print("Another shelf")
 S2 = Shelf(id = 'S2', intial_location = [2,2], map_size = [map_size_x, map_size_y])
+database.update_db(S2.id, S2)
+
+'''
+databse = Database(shelvs = S2)
+database.write_to_db(S2.id, [S2.intial_location[0], S2.intial_location[1], 'P1'])
 
 map.update_objects_locations({S1.id:S1.locations, S2.id:S2.locations})
 map.show_map()
+'''
 
+# Note 2 --> Once a robot object is created, it must be added to the database
 
-R1 = Robot(id = 'R1', intial_location = [3,3], intial_orientation = 'right', speed=1, map_size = [map_size_x, map_size_y])
-R2 = Robot(id = 'R2', intial_location = [0,3], intial_orientation = 'left', speed=1, map_size = [map_size_x, map_size_y])
-R3 = Robot(id = 'R3', intial_location = [0,0], intial_orientation = 'left', speed=1, map_size = [map_size_x, map_size_y])
+R1 = Robot(id = "'R1'", intial_location = [5,11], intial_orientation = 'right', speed=10, map_size = [map_size_x, map_size_y])
+#database.update_db(R1.id, [R1.speed, R1.prev_location[0], R1.prev_location[1], R1.prev_location[0], R1.prev_location[1], 'S1', 15, R1.battery_precentage])
+database.update_db(table="Robots", id=R1.id, parameters={'Speed':R1.speed, 'CurrentLocationX':R1.prev_location[0]})
+database.update_db(table="RobotHealth", id=R1.id, parameters={'BatteryLife':R1.battery_precentage})
 
+#R2 = Robot(id = 'R2', intial_location = [0,3], intial_orientation = 'left', speed=1, map_size = [map_size_x, map_size_y])
+#R3 = Robot(id = 'R3', intial_location = [0,0], intial_orientation = 'left', speed=1, map_size = [map_size_x, map_size_y])
+
+'''
 map.update_objects_locations({R1.id:R1.locations, R2.id:R2.locations, R3.id:R3.locations})
 map.show_map()
 
@@ -36,17 +57,5 @@ for i in range(5):
 	control.steps_map(map)
 	
 
-'''
-R1.move_forward()
-R2.move_forward()
-map.update_objects_locations({R1.id:R1.locations, R2.id:R2.locations})
-map.show_map()
-
-
-R1.rotate_90_degree_clock_wise()
-R2.rotate_90_degree_anti_clock_wise()
-R1.move_forward()
-R2.move_forward()
-map.update_objects_locations({R1.id:R1.locations, R2.id:R2.locations})
-map.show_map()
+# Note 3 --> Once any object is updated, it must be updated in the database
 '''
