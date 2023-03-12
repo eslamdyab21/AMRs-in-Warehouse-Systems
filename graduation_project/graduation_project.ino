271b142d0781b584a698f1ceeb78b1512c358b86
Islam Ashraf <eslamashraf20003@gmail.com>
2023-03-12 02:20:14 +0200

    Added basic ROS topics - fixes #17

--- graduation_project/graduation_project.ino
+++ graduation_project/graduation_project.ino
@@ -1,5 +1,15 @@
 #define __STM32F1__
 #include <ros.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/Byte.h>
+
+
+// REALLY IMPORTANT TO DECREASE INPUT AND OUTPUT BUFFER SIZE, here set to 100 bytes each
+ros::NodeHandle_<ArduinoHardware, 1, 4, 2048, 2048> nh;
+
 
 // left motor pins on cytron
 #define dir_left_motor PA5 
@@ -25,7 +35,7 @@
 #define pin_move_lifitng PB12 
 #define pinA_lifting PB13 
 #define pinB_lifting PB14
- 
+
 // Sensors Pins
 #define voltage_sensor PB0 
 #define current_sensor PB1 
@@ -34,16 +44,17 @@
 #define SCL_pin PB6 
 #define SDA_pin PB7 
 
+
+/*---------------- Variables declarations ----------------*/
+
 // counts for  encoders
 int count_left = 0, count_right = 0, count_lift = 0; 
-double prev_time = 0, speed_timer = 1000;
-double action_timer = 10, last_action = 0;
 
 // Speeds for motors 
 float speed_left = 0, speed_right = 0;
 
 // IMU variables 
-float yaw = 0, pitch = 0, roll = 0;
+float yaw_angle = 0, pitch_angle = 0, roll_angle = 0;
 
 // States for encoders 
 int current_state_l = 0, current_state_r = 0, current_state_lift = 0;
@@ -56,6 +67,29 @@ float y = 0, m = 0, epsi = 0.8;
 
 // PWMs for motors
 int pwm_left = 0, pwm_right = 0;
+
+// timers variables
+double prev_time = 0, speed_timer = 1000;
+double action_timer = 10, last_action = 0;
+double imu_timer = 50, prev_imu_timer = 0;
+double feedback_timer = 1000, prev_feedback_timer = 0;
+  
+/*---------------- Publishers for ros ----------------*/
+
+// Publisher for feedback
+std_msgs::Int16MultiArray feedback;
+ros::Publisher pub_feedback("feedback", &feedback);
+// Publisher for yaw 
+std_msgs::Float32 yaw;
+ros::Publisher pub_yaw("yaw", &yaw);
+// Publisher for pitch
+std_msgs::Float32 pitch;
+ros::Publisher pub_pitch("pitch", &pitch);
+// Publisher for roll
+std_msgs::Float32 roll;
+ros::Publisher pub_roll("roll", &roll);
+
+/*---------------- Interrubts functions ----------------*/
 
 void funcL(){
    current_state_l = digitalRead(pinA_encoder_left);
@@ -108,6 +142,17 @@ void funcR(){
 
 }
 
+// Callback functions
+
+void setSpeeds(const std_msgs::Int16MultiArray& speeds)
+{
+  short int *got = speeds.data;
+  y = got[0];
+  m = got[1];
+}
+
+ros::Subscriber<std_msgs::Int16MultiArray> sub_speeds("speeds", &setSpeeds );
+
 void backward(){
   analogWrite(pwm_left_motor,100);
   analogWrite(pwm_right_motor,100);
@@ -141,7 +186,10 @@ void stop_(){
   digitalWrite(dir_left_motor, HIGH);
   digitalWrite(dir_right_motor, LOW);
 }
+
+
 void setup() {
+  // Pin mode declarations
   pinMode(dir_left_motor, OUTPUT);
   pinMode(pwm_left_motor, OUTPUT);
   pinMode(dir_right_motor, OUTPUT);
@@ -157,12 +205,24 @@ void setup() {
   pinMode(pinB_encoder_lifting,INPUT);
   pinMode(voltage_sensor, INPUT);
   pinMode(current_sensor, INPUT);
+
+  //ISR for encoders declarations
   attachInterrupt(digitalPinToInterrupt(pinA_encoder_right), funcR,CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinA_encoder_left), funcL,CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinA_encoder_lifting), funcLift,CHANGE);
+
+  // Ros nodes declarations
+  nh.initNode();
+  nh.subscribe(sub_speeds);
+  nh.advertise(pub_feedback);
+  nh.advertise(pub_yaw);
+  nh.advertise(pub_pitch);
+  nh.advertise(pub_roll);
+   
 }
 
 void loop() {
+  // Calculating linear speeds for motors
   if ((millis()-prev_time) > speed_timer){
     speed_left = (count_left/450)*0.035;
     count_left = 0;
@@ -172,8 +232,9 @@ void loop() {
     
     prev_time = millis();
   }
-  
-  if ((millis()-last_action) > action_timer){
+
+  // calculating PWM for motors
+  if ((millis() - last_action) > action_timer){
     pwm_left = pwm_left*epsi + (y-m)*(1 - epsi);
     pwm_right = pwm_right*epsi + (y+m)*(1 - epsi);
     
@@ -194,7 +255,31 @@ void loop() {
       analogWrite(pwm_right_motor,abs(pwm_right));
       digitalWrite(dir_right_motor, LOW);
     }
+    last_action = millis();
     
   }
+
+  // Sending IMU variables using ROS
+  if ((millis() - prev_imu_timer) > imu_timer)
+  {
+    yaw.data = yaw_angle;
+    pub_yaw.publish(&yaw);
+    pitch.data = pitch_angle;
+    pub_pitch.publish(&pitch);
+    roll.data = roll_angle;
+    pub_roll.publish(&roll);
+    prev_imu_timer = millis();
+  }
+
+  // Sending feedback values of motors using ROS
+  if ((millis() - prev_feedback_timer) > feedback_timer)
+  {
+    feedback.data_length = 2;
+    feedback.data[0] = abs(pwm_left);
+    feedback.data[1] = abs(pwm_right);
+    
+    pub_feedback.publish(&feedback);
+  }
   
+  nh.spinOnce();
 }
