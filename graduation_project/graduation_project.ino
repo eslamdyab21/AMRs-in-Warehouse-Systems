ea82f75b5fe432b367146d6bcd844bb519f2bcc7
Islam Ashraf <eslamashraf20003@gmail.com>
2023-03-23 03:12:06 +0200

    Moved IMu and sensors to the other stm - fixes #17

--- graduation_project/graduation_project.ino
+++ graduation_project/graduation_project.ino
@@ -8,7 +8,7 @@
 
 
 // REALLY IMPORTANT TO DECREASE INPUT AND OUTPUT BUFFER SIZE, here set to 100 bytes each
-ros::NodeHandle_<ArduinoHardware, 1, 4, 2048, 2048> nh;
+ros::NodeHandle_<ArduinoHardware, 2, 1, 2048, 2048> nh;
 
 
 // left motor pins on cytron
@@ -36,15 +36,6 @@ ros::NodeHandle_<ArduinoHardware, 1, 4, 2048, 2048> nh;
 #define pinA_lifting PB13 
 #define pinB_lifting PB14
 
-// Sensors Pins
-#define voltage_sensor PB0 
-#define current_sensor PB1 
-// IMU pins 
-
-#define SCL_pin PB6 
-#define SDA_pin PB7 
-
-
 /*---------------- Variables declarations ----------------*/
 
 // counts for  encoders
@@ -54,7 +45,7 @@ int count_left = 0, count_right = 0, count_lift = 0;
 float speed_left = 0, speed_right = 0;
 
 // IMU variables 
-float yaw_angle = 0, pitch_angle = 0, roll_angle = 0;
+float yaw_angle = 0;
 
 // States for encoders 
 int current_state_l = 0, current_state_r = 0, current_state_lift = 0;
@@ -69,9 +60,8 @@ float y = 0, m = 0, epsi = 0.8;
 int pwm_left = 0, pwm_right = 0;
 
 // timers variables
-double prev_time = 0, speed_timer = 1000;
+double prev_time = 0, speed_timer = 100;
 double action_timer = 10, last_action = 0;
-double imu_timer = 50, prev_imu_timer = 0;
 double feedback_timer = 1000, prev_feedback_timer = 0;
   
 /*---------------- Publishers for ros ----------------*/
@@ -79,15 +69,7 @@ double feedback_timer = 1000, prev_feedback_timer = 0;
 // Publisher for feedback
 std_msgs::Int16MultiArray feedback;
 ros::Publisher pub_feedback("feedback", &feedback);
-// Publisher for yaw 
-std_msgs::Float32 yaw;
-ros::Publisher pub_yaw("yaw", &yaw);
-// Publisher for pitch
-std_msgs::Float32 pitch;
-ros::Publisher pub_pitch("pitch", &pitch);
-// Publisher for roll
-std_msgs::Float32 roll;
-ros::Publisher pub_roll("roll", &roll);
+
 
 /*---------------- Interrubts functions ----------------*/
 
@@ -150,8 +132,14 @@ void setSpeeds(const std_msgs::Int16MultiArray& speeds)
   y = got[0];
   m = got[1];
 }
+void setYaw(const std_msgs::Float32& yaw)
+{
+  yaw_angle = yaw.data;
+
+}
 
 ros::Subscriber<std_msgs::Int16MultiArray> sub_speeds("speeds", &setSpeeds );
+ros::Subscriber<std_msgs::Float32> sub_yaw("yaw", &setYaw );
 
 void backward(){
   analogWrite(pwm_left_motor,100);
@@ -203,8 +191,6 @@ void setup() {
   pinMode(pinB_encoder_left,INPUT);
   pinMode(pinA_encoder_lifting,INPUT);
   pinMode(pinB_encoder_lifting,INPUT);
-  pinMode(voltage_sensor, INPUT);
-  pinMode(current_sensor, INPUT);
 
   //ISR for encoders declarations
   attachInterrupt(digitalPinToInterrupt(pinA_encoder_right), funcR,CHANGE);
@@ -214,20 +200,18 @@ void setup() {
   // Ros nodes declarations
   nh.initNode();
   nh.subscribe(sub_speeds);
+   nh.subscribe(sub_yaw);
   nh.advertise(pub_feedback);
-  nh.advertise(pub_yaw);
-  nh.advertise(pub_pitch);
-  nh.advertise(pub_roll);
-   
+  
 }
 
 void loop() {
   // Calculating linear speeds for motors
   if ((millis()-prev_time) > speed_timer){
-    speed_left = (count_left/450)*0.035;
+    speed_left = (count_left/450)*0.035*10;
     count_left = 0;
 
-    speed_right = (count_right/450)*0.035;
+    speed_right = (count_right/450)*0.035*10;
     count_right = 0;
     
     prev_time = millis();
@@ -259,17 +243,6 @@ void loop() {
     
   }
 
-  // Sending IMU variables using ROS
-  if ((millis() - prev_imu_timer) > imu_timer)
-  {
-    yaw.data = yaw_angle;
-    pub_yaw.publish(&yaw);
-    pitch.data = pitch_angle;
-    pub_pitch.publish(&pitch);
-    roll.data = roll_angle;
-    pub_roll.publish(&roll);
-    prev_imu_timer = millis();
-  }
 
   // Sending feedback values of motors using ROS
   if ((millis() - prev_feedback_timer) > feedback_timer)
