28b4d60fa0b9fc00eb3d1c5553242195cec85b23
eslamdyab21 <eslamdyab22@gmail.com>
2023-04-25 02:09:02 +0200

    add move and set_speed functions in ros-comm - fixes #53

--- ROS-communication/ROS_communication.py
+++ ROS-communication/ROS_communication.py
@@ -18,5 +18,16 @@ class ROS_communication():
         self.movement = rospy.Publisher(f'{robot_id}_movement', String, queue_size=10)
         self.speed = rospy.Publisher(f'{robot_id}_speed', Float32, queue_size=10)
 
+    
+    def move(self, movement):
+        self.movement.publish(movement)
+    
 
+    def set_speed(self, speed):
+        speed = Float32(speed)
+        self.movement.publish(speed)
+
+
+    def close(self):
+        rospy.signal_shutdown('break')
       
