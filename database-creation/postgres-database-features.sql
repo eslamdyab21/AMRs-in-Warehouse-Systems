6b37b5fcfbdc363ecf75178b08526066b83c3021
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-04-26 01:38:44 +0200

    removed the NewOrder trigger and added a new trigger to check the number of order per shelf -fixes #23

--- database-creation/postgres-database-features.sql
+++ database-creation/postgres-database-features.sql
@@ -1,69 +1,43 @@
 /*
 TRIGGERS:
 ---------
-1. Notify Me
-    This trigger does only one task for each new order:
-        Add a notification to the Notifications table. It maps the products in this order with their shelves
-
-2. New Order
+1. New Order
     This trigger does 3 task for each new order:
         1. Decrease the number of items in stock in Products table by the quantity of each product
-        2. Mark the shelves which store the products in this order as HavingOrder shelves (isHavingOrder = 1)
+        2. Increment the number of orders for each shelf by 1 it the order contains the product that this shelf holds
         3. Calculate the total cost of the order
 
-3. ItemsInStockUpdates
+2. ItemsInStockUpdates
     This tigger does 2 tasks for each update on products table:
         1. If ItemsInStock < 10: Sends a notification with the product and its remaining number of items
         2. If ItemsInStock = 0: Sends a notification that the product is out of stock
+
+3. CheckOrderStatus
+    This tigger does only one task for each update on orders_details table:
+        1. Decrement the number of orders on each shelf once the order is marked as "Completed"
 */
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Trigger number 1 : NotifyMe
-CREATE OR REPLACE FUNCTION notify_me_function()
-RETURNS TRIGGER AS
-$$
-DECLARE
-    shelves_products TEXT := (SELECT STRING_AGG(OD.Quantity || ' items of ' || OD.ProductID || ' from ' || S.ShelfID, ', ')
-                                FROM Orders_Details AS OD
-                                INNER JOIN Shelves AS S
-                                    ON OD.ProductID = S.ProductID
-                                WHERE OrderID = NEW.OrderID);
-BEGIN
-    -- Send a notification with the products and their shelves
-    INSERT INTO Notifications(Notification)
-    VALUES(CONCAT('There is a new order with ', shelves_products, '.'));
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER notify_me_trigger
-AFTER INSERT
-ON Orders_Details
-FOR EACH STATEMENT
-EXECUTE FUNCTION notify_me_function();
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
--- Trigger number 2 : NewOrder
+-- Trigger number 1 : NewOrder
 CREATE OR REPLACE FUNCTION new_order_function()
 RETURNS TRIGGER AS
 $$
 DECLARE
-    total_cost FLOAT := (SELECT SUM(OD.Quantity * P.Price) AS TotalCost
-                            FROM Orders_Details AS OD
+    total_cost FLOAT := (SELECT SUM(O.Quantity * P.Price) AS TotalCost
+                            FROM Orders AS O
                             INNER JOIN Products AS P
-                                ON P.ProductID = OD.ProductID
+                                ON P.ProductID = O.ProductID
                             WHERE OrderID = NEW.OrderID);
 BEGIN
     -- Update the number of items in stock of each product
     UPDATE Products SET ItemsInStock = ItemsInStock - NEW.Quantity WHERE ProductID = NEW.ProductID;
 
-    -- Mark the shelves which store the products as having orders
-    UPDATE Shelves SET isHavingOrder = TRUE WHERE ProductID = New.ProductID;
+    -- Update the number of orders in shelves
+    UPDATE Shelves SET NumOfOrders = NumOfOrders + 1 WHERE ProductID = NEW.ProductID;
 
     -- Calculate the total cost
-    UPDATE Orders SET TotalCost = @total_cost;
+    UPDATE Orders_Details SET TotalCost = @total_cost;
 
     RETURN NEW;
 END;
@@ -71,13 +45,13 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER new_order_trigger
 AFTER INSERT
-ON Orders_Details
+ON Orders
 FOR EACH ROW
 EXECUTE FUNCTION new_order_function();
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Trigger number 3 : ItemsInStockUpdates
+-- Trigger number 2 : ItemsInStockUpdates
 CREATE OR REPLACE FUNCTION items_in_stock_updates_function()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -91,8 +65,10 @@ BEGIN
     END IF;
 
     -- Sends the notification
-    INSERT INTO Notifications(Notification)
-        VALUES(items_in_stock_notification);
+    IF items_in_stock_notification IS NOT NULL THEN
+        INSERT INTO Notifications(Notification)
+            VALUES(items_in_stock_notification);
+    END IF;
 
     RETURN NEW;
 END;
@@ -102,5 +78,25 @@ CREATE TRIGGER items_in_stock_updates_trigger
 AFTER UPDATE ON Products
 FOR EACH ROW
 EXECUTE FUNCTION items_in_stock_updates_function();
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Trigger number 3 : CheckOrderStatus
+CREATE OR REPLACE FUNCTION check_order_status_function()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.OrderStatus = 'Completed' THEN
+        UPDATE Shelves SET NumOfOrders = NumOfOrders - 1
+            WHERE ProductID IN (SELECT ProductID FROM Orders WHERE OrderID = NEW.OrderID);
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_order_status_trigger
+AFTER UPDATE ON Orders_Details
+FOR EACH ROW
+EXECUTE FUNCTION check_order_status_function();
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
