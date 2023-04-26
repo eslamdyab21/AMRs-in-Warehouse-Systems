5259eeaabe2b43551be5e3c3b030aa9668b12a46
eslamdyab21 <eslamdyab22@gmail.com>
2023-04-26 22:27:13 +0200

    add a connect_to_postgress_db function - fixes #5

--- dashboard-web-application/server/routes/api/Database.js
+++ dashboard-web-application/server/routes/api/Database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const dotenv = require("dotenv");
+const { Client } = require('pg');
 
 class database{
     /*
@@ -13,7 +14,31 @@ class database{
 	}
 	
 	//methods
-    connect_to_db(){
+    connect_to_postgress_db(){
+
+        dotenv.config({ path: 'routes/api/.env' })
+        this.db = new Client({
+            host : process.env.POSTGRES_HOST,
+            user : process.env.POSTGRES_USER,
+            password : process.env.POSTGRES_PASSWORD,
+            database : process.env.POSTGRES_DATABASE,
+            port: process.env.POSTGRES_PORT,
+          });
+
+        this.db.connect((err) => {
+            if (err){
+                console.log('problem connecting to database....')
+                throw err
+            }
+            console.log(`connected to ${process.env.MYSQL_DATABASE} database`)
+        })
+
+        return this.db
+    }
+
+
+    
+    connect_to_mysql_db(){
         /*
         connect_to_db function is responsible of establishing the connection between the database and this python code
         using a cursor and defining the database which we need to be in use
