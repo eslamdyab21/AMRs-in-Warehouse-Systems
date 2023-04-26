dce6361aae4b26a2fe0b940a7ef0e8be20c746fa
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-04-26 03:39:01 +0200

    added some test queries -fixes #23

--- database-creation/testing-queries.sql
+++ database-creation/testing-queries.sql
@@ -5,9 +5,12 @@ INSERT INTO Customers VALUES('C1', 'menna@gmail', 'mennapassword', 'Mennatallah 
 INSERT INTO Products VALUES('P1', 'Hair Shampoo', 250, 'It is a hair product. It contains Sulfates', 500, 'URL1', 'URL2', 'URL3'),
                             ('P2', 'Hair Conditioner', 300, 'It is a hair product. It controls frizzness', 500, 'URL1', 'URL2', 'URL3');
 
-INSERT INTO Shelves VALUES('S1', 0, 5, 'P1', 0), ('S2', 0, 10, 'P2', 0); -- mysql
-INSERT INTO Shelves VALUES('S1', 0, 5, 'P1', 0), ('S2', 0, 10, 'P2', 0); -- postgres
+INSERT INTO Shelves(ShelfID, Location_X, Location_Y, ProductID, NumOfOrders)
+VALUES('S1', 0, 5, 'P1', 0), ('S2', 0, 10, 'P2', 0);
 
 INSERT INTO Orders VALUES('O1', 'P1', 5), ('O1', 'P2', 7); -- first for mysql
 
 INSERT INTO Orders_Details VALUES('O1', 'C1', NULL, NOW(), '+201234567890', 'Alexandria', 'Cash on Delivery', 'New'); -- first for postgres
+
+INSERT INTO Robots(RobotID, Speed, BatteryPercentage, CurrentLocation_X, CurrentLocation_Y, isCharging, ShelfID)
+VALUES('R1', 90, 100, 5, 5, False, 'S1'), ('R2', 80, 50, 10, 10, False, 'S2');
