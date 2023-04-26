8fe772cfdbe64a10476859d9f221f508bd0702fe
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-04-26 22:48:50 +0200

    edited the testing queries to be inserted in order -fixes #23

--- database-creation/testing-queries.sql
+++ database-creation/testing-queries.sql
@@ -1,4 +1,4 @@
--- Inserting queries
+-- Inserting queries: The following queries has to be inserted in order
 
 INSERT INTO Customers VALUES('C1', 'menna@gmail', 'mennapassword', 'Mennatallah Mamdouh', 'Female');
 
@@ -8,9 +8,9 @@ INSERT INTO Products VALUES('P1', 'Hair Shampoo', 250, 'It is a hair product. It
 INSERT INTO Shelves(ShelfID, Location_X, Location_Y, ProductID, NumOfOrders)
 VALUES('S1', 0, 5, 'P1', 0), ('S2', 0, 10, 'P2', 0);
 
-INSERT INTO Orders VALUES('O1', 'P1', 5), ('O1', 'P2', 7); -- first for mysql
+INSERT INTO Orders_Details VALUES('O1', 'C1', NULL, NOW(), '+201234567890', 'Alexandria', 'Cash on Delivery', 'New');
 
-INSERT INTO Orders_Details VALUES('O1', 'C1', NULL, NOW(), '+201234567890', 'Alexandria', 'Cash on Delivery', 'New'); -- first for postgres
+INSERT INTO Orders VALUES('O1', 'P1', 5), ('O1', 'P2', 7);
 
 INSERT INTO Robots(RobotID, Speed, BatteryPercentage, CurrentLocation_X, CurrentLocation_Y, isCharging, ShelfID)
 VALUES('R1', 90, 100, 5, 5, False, 'S1'), ('R2', 80, 50, 10, 10, False, 'S2');
