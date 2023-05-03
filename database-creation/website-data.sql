1743102a4ff4f5ace3d36540efc421069759c26f
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-05-03 23:39:11 +0300

    added the products data on the website -fixes #23

---
+++ database-creation/website-data.sql
@@ -0,0 +1,18 @@
+-- Products Data
+
+INSERT INTO Products
+    (ProductID, ProductName, Description, Price, ItemsInStock, Image_1, Image_2, Image_3)
+VALUES
+    ('P1', 'Laptop', 'Lenovo\r\nIdeaPad 3 15ITL6/Intel® Core™ i3-1115G4/4GB/256 GB SSD/Intel® UHD Graphics/15.6\" HD/Dos/Arctic Grey/(82H802ACED)',
+            13000, 10, 'Laptop-1.webp', 'Laptop-2.webp', 'Laptop-3.webp'),
+
+    ('P2', 'Mouse', 'Razer DeathAdder V2 - Wired USB Gaming Mouse with Optical Mouse Switches (Focus+ 20K Optical Sensor, 8 Programmable Buttons, 5 On-Board Memory Profiles, Optical Mouse Switch) Black',
+            1899, 10, 'Mouse-1.webp', 'Mouse-2.webp', 'Mouse-3.webp'),
+
+    ('P3', 'Camera', 'Canon PowerShot SX50 HS 12.1 MP Digital Camera with 50x Wide-Angle Optical Image Stabilized Zoom',
+            15000, 10, 'camera-1.webp', 'camera-2.webp', 'camera-3.webp'),
+
+    ('P4', 'Mixer', 'Croma 500W Mixer Grinder with 3 Stainless Steel Leak-proof Jars, 3 speed & Pulse function, 2 years warranty (CRAK4184, White & Purple)',
+            844, 10, 'Mixer-1.webp', 'Mixer-2.webp', 'Mixer-3.webp');
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
