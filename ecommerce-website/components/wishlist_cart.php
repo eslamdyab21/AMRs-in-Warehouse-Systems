56d3e89c7d23dd49502e3153e991282c9fb12cb2
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-05-04 02:52:16 +0300

    connected the wishlist page and the wishlist-cart component to the database -fixes #6

--- ecommerce-website/components/wishlist_cart.php
+++ ecommerce-website/components/wishlist_cart.php
@@ -1,12 +1,11 @@
 <?php
 
 if(isset($_POST['add_to_wishlist'])){
-
    if($user_id == ''){
       header('location:user_login.php');
    }else{
 
-      $pid = $_POST['pid'];
+      $pid = $_POST['id'];
       $pid = filter_var($pid, FILTER_SANITIZE_STRING);
       $name = $_POST['name'];
       $name = filter_var($name, FILTER_SANITIZE_STRING);
@@ -15,20 +14,26 @@ if(isset($_POST['add_to_wishlist'])){
       $image = $_POST['image'];
       $image = filter_var($image, FILTER_SANITIZE_STRING);
 
-      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM Wishlist AS W
+                                                INNER JOIN Products AS P
+                                                   ON P.ProductID = W.ProductID
+                                                WHERE P.ProductName = ? AND W.CustomerID = ?");
       $check_wishlist_numbers->execute([$name, $user_id]);
 
-      $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+      $check_cart_numbers = $conn->prepare("SELECT * FROM Cart AS C
+                                             INNER JOIN Products AS P
+                                                ON P.ProductID = C.ProductID
+                                             WHERE P.ProductName = ? AND C.CustomerID = ?");
       $check_cart_numbers->execute([$name, $user_id]);
 
       if($check_wishlist_numbers->rowCount() > 0){
-         $message[] = 'already added to wishlist!';
+         $message[] = 'Already added to wishlist!';
       }elseif($check_cart_numbers->rowCount() > 0){
-         $message[] = 'already added to cart!';
+         $message[] = 'Already added to cart!';
       }else{
-         $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
-         $insert_wishlist->execute([$user_id, $pid, $name, $price, $image]);
-         $message[] = 'added to wishlist!';
+         $insert_wishlist = $conn->prepare("INSERT INTO Wishlist(CustomerID, ProductID) VALUES(?,?)");
+         $insert_wishlist->execute([$user_id, $pid]);
+         $message[] = 'Added to wishlist!';
       }
 
    }
@@ -52,24 +57,30 @@ if(isset($_POST['add_to_cart'])){
       $qty = $_POST['qty'];
       $qty = filter_var($qty, FILTER_SANITIZE_STRING);
 
-      $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+      $check_cart_numbers = $conn->prepare("SELECT * FROM Cart AS C
+                                             INNER JOIN Products AS P
+                                                ON P.ProductID = C.ProductID
+                                             WHERE P.ProductName = ? AND C.CustomerID = ?");
       $check_cart_numbers->execute([$name, $user_id]);
 
       if($check_cart_numbers->rowCount() > 0){
-         $message[] = 'already added to cart!';
+         $message[] = 'Already added to cart!';
       }else{
 
-         $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $check_wishlist_numbers = $conn->prepare("SELECT * FROM Wishlist AS W
+                                                   INNER JOIN Products AS P
+                                                      ON P.ProductID = W.ProductID
+                                                   WHERE P.ProductName = ? AND W.CustomerID = ?");
          $check_wishlist_numbers->execute([$name, $user_id]);
 
          if($check_wishlist_numbers->rowCount() > 0){
-            $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+            $delete_wishlist = $conn->prepare("DELETE FROM Wishlist WHERE ProductName = ? AND CustomerID = ?");
             $delete_wishlist->execute([$name, $user_id]);
          }
 
-         $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
-         $insert_cart->execute([$user_id, $pid, $name, $price, $qty, $image]);
-         $message[] = 'added to cart!';
+         $insert_cart = $conn->prepare("INSERT INTO Cart(CustomerID, ProductID) VALUES(?,?)");
+         $insert_cart->execute([$user_id, $pid]);
+         $message[] = 'Added to cart!';
          
       }
 
--- ecommerce-website/wishlist.php
+++ ecommerce-website/wishlist.php
@@ -4,8 +4,8 @@ include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
+if(isset($_SESSION['CustomerID'])){
+   $user_id = $_SESSION['CustomerID'];
 }else{
    $user_id = '';
    header('location:user_login.php');
@@ -14,13 +14,13 @@ if(isset($_SESSION['user_id'])){
 include 'components/wishlist_cart.php';
 
 if(isset($_POST['delete'])){
-   $wishlist_id = $_POST['wishlist_id'];
-   $delete_wishlist_item = $conn->prepare("DELETE FROM `wishlist` WHERE id = ?");
-   $delete_wishlist_item->execute([$wishlist_id]);
+   $product_id = $_POST['pid'];
+   $delete_wishlist_item = $conn->prepare("DELETE FROM Wishlist WHERE CustomerID = ? AND ProductID = ?");
+   $delete_wishlist_item->execute([$user_id, $product_id]);
 }
 
 if(isset($_GET['delete_all'])){
-   $delete_wishlist_item = $conn->prepare("DELETE FROM `wishlist` WHERE user_id = ?");
+   $delete_wishlist_item = $conn->prepare("DELETE FROM Wishlist WHERE CustomerID = ?");
    $delete_wishlist_item->execute([$user_id]);
    header('location:wishlist.php');
 }
@@ -54,21 +54,24 @@ if(isset($_GET['delete_all'])){
 
    <?php
       $grand_total = 0;
-      $select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
+      $select_wishlist = $conn->prepare("SELECT W.ProductID, P.ProductName, P.Price, P.Image_1 FROM Wishlist AS W
+                                          INNER JOIN Products AS P
+                                             ON P.ProductID = W.ProductID
+                                          WHERE CustomerID = ?");
       $select_wishlist->execute([$user_id]);
       if($select_wishlist->rowCount() > 0){
          while($fetch_wishlist = $select_wishlist->fetch(PDO::FETCH_ASSOC)){
             $grand_total += $fetch_wishlist['price'];  
    ?>
    <form action="" method="post" class="box">
-      <input type="hidden" name="pid" value="<?= $fetch_wishlist['pid']; ?>">
-      <input type="hidden" name="wishlist_id" value="<?= $fetch_wishlist['id']; ?>">
-      <input type="hidden" name="name" value="<?= $fetch_wishlist['name']; ?>">
+      <input type="hidden" name="pid" value="<?= $fetch_wishlist['productid']; ?>">
+      
+      <input type="hidden" name="name" value="<?= $fetch_wishlist['productname']; ?>">
       <input type="hidden" name="price" value="<?= $fetch_wishlist['price']; ?>">
-      <input type="hidden" name="image" value="<?= $fetch_wishlist['image']; ?>">
-      <a href="quick_view.php?pid=<?= $fetch_wishlist['pid']; ?>" class="fas fa-eye"></a>
-      <img src="uploaded_img/<?= $fetch_wishlist['image']; ?>" alt="">
-      <div class="name"><?= $fetch_wishlist['name']; ?></div>
+      <input type="hidden" name="image" value="<?= $fetch_wishlist['image_1']; ?>">
+      <a href="quick_view.php?pid=<?= $fetch_wishlist['productid']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/<?= $fetch_wishlist['image_1']; ?>" alt="">
+      <div class="name"><?= $fetch_wishlist['productname']; ?></div>
       <div class="flex">
          <div class="price">$<?= $fetch_wishlist['price']; ?>/-</div>
          <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
