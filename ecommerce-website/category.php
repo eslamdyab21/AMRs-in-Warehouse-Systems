af9d6d0dfcea9e3cfd515a107196222b8722f4de
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-05-04 18:28:39 +0300

    connected the category page to the database -fixes #6

--- ecommerce-website/category.php
+++ ecommerce-website/category.php
@@ -4,8 +4,8 @@ include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
+if(isset($_SESSION['CustomerID'])){
+   $user_id = $_SESSION['CustomerID'];
 }else{
    $user_id = '';
 };
@@ -41,26 +41,26 @@ include 'components/wishlist_cart.php';
 
    <?php
      $category = $_GET['category'];
-     $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$category}%'"); 
+     $select_products = $conn->prepare("SELECT * FROM Products WHERE LOWER(ProductName) LIKE LOWER('%{$category}%')"); 
      $select_products->execute();
      if($select_products->rowCount() > 0){
       while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
    ?>
    <form action="" method="post" class="box">
-      <input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
-      <input type="hidden" name="name" value="<?= $fetch_product['name']; ?>">
+      <input type="hidden" name="pid" value="<?= $fetch_product['productid']; ?>">
+      <input type="hidden" name="name" value="<?= $fetch_product['productname']; ?>">
       <input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
-      <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
+      <input type="hidden" name="image" value="<?= $fetch_product['image_1']; ?>">
       <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
-      <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
-      <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
-      <div class="name"><?= $fetch_product['name']; ?></div>
+      <a href="quick_view.php?pid=<?= $fetch_product['productid']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/<?= $fetch_product['image_1']; ?>" alt="">
+      <div class="name"><?= $fetch_product['productname']; ?></div>
       <div class="flex">
          <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
-         <input type="number" name="qty" class="qty" min="1" max="<?php echo $fetch_product['InStock']; ?>" onkeypress="if(this.value.length == 2) return false;" value="1">
+         <input type="number" name="qty" class="qty" min="1" max="<?php echo $fetch_product['itemsinstock']; ?>" onkeypress="if(this.value.length == 2) return false;" value="1">
       </div>
       <?php
-         if($fetch_product['InStock']>0)
+         if($fetch_product['itemsinstock']>0)
          { ?>
             <input type="submit" value="add to cart" class="btn" name="add_to_cart">
             <?php
