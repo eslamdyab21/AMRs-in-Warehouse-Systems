440a2b44251a89b70c95b86f142865ebcb97bd7a
eslamdyab21 <eslamdyab22@gmail.com>
2023-06-13 19:52:03 +0300

    center sidebar Typography - fixes #5

--- dashboard-web-application/client/src/components/Sidebar.jsx
+++ dashboard-web-application/client/src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const navItems = [
     icon: <HomeOutlined />,
   },
   {
-    text: "Warehouse data",
+    text: "Data",
     icon: null,
   },
   {
@@ -125,7 +125,7 @@ const Sidebar = ({
             <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
-                  <Typography variant="h4" fontWeight="bold">
+                  <Typography align="center" variant="h4" fontWeight="bold">
                     Warehouse Administration
                   </Typography>
                 </Box>
