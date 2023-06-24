c9d4e57de78657bf4e97ebc4004795a3c8e8242e
eslamdyab21 <eslamdyab22@gmail.com>
2023-06-24 20:31:39 +0300

    rename Master_db to two nodes one for querying and one for updating with ros - fixes #53

--- ROS-communication/DB_query_node.py
+++ ROS-communication/DB_query_node.py
@@ -7,25 +7,28 @@ from std_msgs.msg import String
 import time
 import ast
 from Map2D import Map2D
+from Database import Database
+from Logger import Logger
 
-
-
-class master():
+class DB_query_node():
 
     def __init__(self, local_ip='127.0.0.1', roscore_ip='127.0.0.1'):
+        self.logger = Logger()
+
+        self.database = Database(self.logger)
 
         roscore_port = 11311
         os.environ['ROS_IP'] = local_ip
         os.environ['ROS_HOSTNAME'] = local_ip
         os.environ['ROS_MASTER_URI'] = 'http://' + roscore_ip + ':' + str(roscore_port)
 
-        rospy.init_node('master', anonymous=True)
+        rospy.init_node('DB_query_node', anonymous=True)
 
         # dictionary of shelves orders and their locations
         # {shelf_id:{movement_status:'moving or waiting', received_order_status:'True or False'}, ....}
         rospy.Subscriber("ros_shelves_status", String, self.ros_shelves_status_callback)
         rospy.Subscriber("ros_2dmap", String, self.ros_2dmap_callback)
-        rospy.Subscriber("ros_robots_status", String, self.ros_robots_status_callback)
+        # rospy.Subscriber("ros_robots_status", String, self.ros_robots_status_callback)
 
         self.ros_assign_robot_2_shelf_topic = rospy.Publisher('ros_assign_robot_2_shelf', String, queue_size=10)
         self.ros_shelves_status_topic = rospy.Publisher('ros_shelves_status', String, queue_size=10)
@@ -39,6 +42,23 @@ class master():
         self.map = Map2D(size_x=map_size_x, size_y=map_size_y)
 
 
+
+    def query_recived_order_shelfs_from_db(self):
+        """
+        query_recived_order_shelfs_from_db function queres shelves order status
+
+        :return shelves_recived_order_list: a list of shelves ids who received orders
+        """
+
+        start_time = time.time()
+        shelves_id_recived_order_list = self.database.query_recived_order_shelfs_id()
+        
+
+        self.logger.log(f'DB_query_node : query_recived_order_shelfs_from_db : {time.time()-start_time} -->')
+
+        return shelves_id_recived_order_list
+    
+    
     def ros_shelves_status_callback(self, data):
         start_time = time.time()
 
@@ -86,11 +106,10 @@ class master():
 
 
     
-master_node = master(local_ip='127.0.0.1', roscore_ip='127.0.0.1')
+db_query_node = DB_query_node(local_ip='127.0.0.1', roscore_ip='127.0.0.1')
 
 while True:
+    time.sleep(2)
+    db_query_node.query_recived_order_shelfs_from_db()
 
-    for i in range(1000):
-        x = i
-        x = x + 1
 
---
+++ ROS-communication/DB_update_node.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import rospy
+import os
+from std_msgs.msg import String
+import time
+import ast
+from Map2D import Map2D
+from Database import Database
+from Logger import Logger
+
+class DB_update_node():
+
+    def __init__(self, local_ip='127.0.0.1', roscore_ip='127.0.0.1'):
+        self.logger = Logger()
+
+        self.database = Database(self.logger)
+
+        roscore_port = 11311
+        os.environ['ROS_IP'] = local_ip
+        os.environ['ROS_HOSTNAME'] = local_ip
+        os.environ['ROS_MASTER_URI'] = 'http://' + roscore_ip + ':' + str(roscore_port)
+
+        rospy.init_node('DB_update_node', anonymous=True)
+
+        # dictionary of shelves orders and their locations
+        # {shelf_id:{shelf_id:Sx, movement_status:'moving or waiting', received_order_status:'True or False', 'paired_with_robot':R.id}, ....}
+        rospy.Subscriber("ros_shelves_status", String, self.ros_shelves_status_callback)
+        
+        rospy.Subscriber("ros_robots_status", String, self.ros_robots_status_callback)
+
+        self.ros_assign_robot_2_shelf_topic = rospy.Publisher('ros_assign_robot_2_shelf', String, queue_size=10)
+        self.ros_shelves_status_topic = rospy.Publisher('ros_shelves_status', String, queue_size=10)
+
+        
+
+        self.ros_robots_status_dict = None
+
+    
+    
+    def ros_shelves_status_callback(self, data):
+        start_time = time.time()
+
+        ros_shelves_status_dict = str(data.data)
+        ros_shelves_status_dict = ast.literal_eval(ros_shelves_status_dict)
+
+        for shelf_id,shelf_data in ros_shelves_status_dict.items():
+            self.database.update_db(table="Shelves", id=shelf_id, parameters={"currentlocation_x":shelf_data['locations'][1][0], "currentlocation_y":shelf_data['locations'][1][1]})
+
+        # self.logger.log(f'Control : ros_order_at_shelf_callback : {time.time()-start_time} -->')
+
+
+    def ros_robots_status_callback(self, data):
+        start_time = time.time()
+
+        ros_robots_status_dict = str(data.data)
+        ros_robots_status_dict = ast.literal_eval(ros_robots_status_dict)
+        
+        for robot_id,robot_data in ros_robots_status_dict.items():
+            self.database.update_db(table="Shelves", id=robot_id, 
+            parameters={"currentlocation_x":robot_data['locations'][1][0],
+                        "currentlocation_y":robot_data['locations'][1][1], 
+                        "shelfid":robot_data['paired_with_shelf'], 
+                        "speed":robot_data['speed'],
+                        "batteryPercentage":robot_data['battery']})
+            # ,"isCharging":robot_data['is_charging']
+
+        # self.logger.log(f'Control : ros_robots_movement_status_callback : {time.time()-start_time} -->')
+    
+
+
+    
+db_query_node = DB_update_node(local_ip='127.0.0.1', roscore_ip='127.0.0.1')
+
+while True:
+    rospy.spin()
+
+
