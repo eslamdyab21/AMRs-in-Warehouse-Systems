0c0871dce1eb00a2c109725bf9b846f1b5e007da
eslamdyab21 <eslamdyab22@gmail.com>
2023-06-25 02:49:04 +0300

    add scripts.groovy for ci pipline - fixes #75

---
+++ scripts.groovy
@@ -0,0 +1,29 @@
+def install_node_dependencies() {
+    echo "installing the node application dependencies..."
+    sh 'npm install'
+}
+
+
+
+def create_docker_image() {
+    echo "creating the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:dashboard-backend-dev-0.0 .'
+    }
+} 
+
+
+def push_docker_image() {
+    echo "pushing the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push eslamdyba/amrs-in-warehouse-systems:dashboard-backend-dev-0.0'
+    }
+} 
+
+
+def deploy() {
+    echo 'deploying the application...'
+} 
+
+return this
