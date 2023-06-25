d1fe09246089d4c2623a3911d9f90c70fa848e20
eslamdyab21 <eslamdyab22@gmail.com>
2023-06-25 18:12:19 +0300

    ros_order_at_shelf only the shelf who is not paired with a robot - fixes #53

--- ROS-communication/DB_query_node.py
+++ ROS-communication/DB_query_node.py
@@ -24,22 +24,11 @@ class DB_query_node():
 
         rospy.init_node('DB_query_node', anonymous=True)
 
+        self.ros_order_at_shelf = rospy.Publisher('ros_order_at_shelf', String, queue_size=10)
+
         # dictionary of shelves orders and their locations
-        # {shelf_id:{movement_status:'moving or waiting', received_order_status:'True or False'}, ....}
+        # {shelf_id:{shelf_id:Sx, movement_status:'moving or waiting', received_order_status:'True or False', 'paired_with_robot':R.id}, ....}
         rospy.Subscriber("ros_shelves_status", String, self.ros_shelves_status_callback)
-        rospy.Subscriber("ros_2dmap", String, self.ros_2dmap_callback)
-        # rospy.Subscriber("ros_robots_status", String, self.ros_robots_status_callback)
-
-        self.ros_assign_robot_2_shelf_topic = rospy.Publisher('ros_assign_robot_2_shelf', String, queue_size=10)
-        self.ros_shelves_status_topic = rospy.Publisher('ros_shelves_status', String, queue_size=10)
-
-        
-        map_size_x = 15
-        map_size_y = 15
-        self.ros_robots_status_dict = None
-
-        # Create a 2d map
-        self.map = Map2D(size_x=map_size_x, size_y=map_size_y)
 
 
 
@@ -54,56 +43,23 @@ class DB_query_node():
         shelves_id_recived_order_list = self.database.query_recived_order_shelfs_id()
         
 
+
+        # publishes the shelf who has an order
+        for shelf in shelves_id_recived_order_list:
+            if self.ros_shelves_status_dict[shelf]['paired_with_robot_status'] == False:
+                self.ros_order_at_shelf.publish(shelf)
+        
         self.logger.log(f'DB_query_node : query_recived_order_shelfs_from_db : {time.time()-start_time} -->')
 
-        return shelves_id_recived_order_list
-    
-    
+        
     def ros_shelves_status_callback(self, data):
         start_time = time.time()
 
         ros_shelves_status_dict = str(data.data)
-        ros_shelves_status_dict = ast.literal_eval(ros_shelves_status_dict)
+        self.ros_shelves_status_dict = ast.literal_eval(ros_shelves_status_dict)
 
-        
-        # get all available robots
-        available_robots_for_order = self.get_available_robots_for_order()
-        
+
         # self.logger.log(f'Control : ros_order_at_shelf_callback : {time.time()-start_time} -->')
-
-
-    def ros_robots_movement_status_callback(self, data):
-        start_time = time.time()
-
-        ros_robots_status_dict = str(data.data)
-        self.ros_robots_status_dict = ast.literal_eval(ros_robots_status_dict)
-        
-
-        # self.logger.log(f'Control : ros_robots_movement_status_callback : {time.time()-start_time} -->')
-
-
-    def get_available_robots_for_order(self):
-        available_robots_for_order = []
-
-        for robot_id in self.ros_robots_status_dict.keys():
-            if self.ros_robots_status_dict[robot_id] == 'waiting for order':
-                available_robots_for_order.append(robot_id)
-
-        return available_robots_for_order
-    
-
-    def ros_2dmap_callback(self, data):
-
-        mapp = data.data
-        map_str = mapp.strip()
-        self.map.map = np.asarray(np.matrix(map_str)) 
-        self.map.map = self.map.map.astype('object')
-        self.map.map = np.resize(self.map.map,(self.map.size_x,self.map.size_y))
-        self.map.map = np.squeeze(self.map.map)
-
-        self.map.show_map()
-    
-
 
     
 db_query_node = DB_query_node(local_ip='127.0.0.1', roscore_ip='127.0.0.1')
