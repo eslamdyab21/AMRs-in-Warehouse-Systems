3dee859beb6f94bde51edbcb963ca3c1d65ed700
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-06-25 16:30:34 +0300

    edited the path of the docker file -fixes #48

--- scripts.groovy
+++ scripts.groovy
@@ -1,7 +1,7 @@
 def create_docker_image() {
     echo "creating the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:postgressql-server-dev-0.0 --build-arg POSTGRES_USER=$POSTGRES_USER --build-arg POSTGRES_DB=$POSTGRES_DATABASE --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD docker-with-databases/Dockerfile'
+        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:postgressql-server-dev-0.0 --build-arg POSTGRES_USER=$POSTGRES_USER --build-arg POSTGRES_DB=$POSTGRES_DATABASE --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD docker-with-databases/Dockerfile .'
     }
 } 
 
