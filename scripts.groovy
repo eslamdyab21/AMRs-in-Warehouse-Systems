f3aceb554b7f4dde58bafeee9ff2bbbd4e8ec9e2
eslamdyab21 <eslamdyab22@gmail.com>
2023-06-25 03:18:40 +0300

    update build stage to take env varibles needed for the backend - fixes #75

--- scripts.groovy
+++ scripts.groovy
@@ -8,7 +8,7 @@ def install_node_dependencies() {
 def create_docker_image() {
     echo "creating the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:dashboard-backend-dev-0.0 .'
+        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:dashboard-backend-dev-0.0 --build-arg POSTGRES_HOST=$POSTGRES_HOST --build-arg POSTGRES_USER=$POSTGRES_USER --build-arg POSTGRES_DATABASE=$POSTGRES_DATABASE --build-arg POSTGRES_PORT=$POSTGRES_PORT --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD .'
     }
 } 
 
