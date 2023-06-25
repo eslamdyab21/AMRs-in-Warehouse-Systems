25fd16eef834cd697613c6706d75dee5e5a494ae
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2023-06-25 16:10:47 +0300

    add pipeline for creating and pushing the docker image for the database server -fixes #48

---
+++ scripts.groovy
@@ -0,0 +1,22 @@
+def create_docker_image() {
+    echo "creating the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t eslamdyba/amrs-in-warehouse-systems:postgressql-server-dev-0.0 --build-arg POSTGRES_USER=$POSTGRES_USER --build-arg POSTGRES_DB=$POSTGRES_DATABASE --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD .'
+    }
+} 
+
+
+def push_docker_image() {
+    echo "pushing the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push eslamdyba/amrs-in-warehouse-systems:postgressql-server-dev-0.0'
+    }
+} 
+
+
+def deploy() {
+    echo 'deploying the application...'
+} 
+
+return this
