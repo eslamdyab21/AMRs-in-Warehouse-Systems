d1da21c489ab0c4c1e82eec8be87948a1f6787ed
Islam Ashraf <eslamashraf20003@gmail.com>
2023-07-01 09:14:56 +0200

    Final arduino code with imu - fixes #17

--- Arduino-code-system-with-ROS/graduation_project/graduation_project.ino
+++ Arduino-code-system-with-ROS/graduation_project/graduation_project.ino
@@ -2,14 +2,18 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Int8.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int16MultiArray.h>
 #include <std_msgs/Byte.h>
 #include "PID.h"
 
-// REALLY IMPORTANT TO DECREASE INPUT AND OUTPUT BUFFER SIZE, here set to 100 bytes each
-ros::NodeHandle_<ArduinoHardware, 2, 1, 2048, 2048> nh;
 PID yawPID(1);
+//PID motorLeft(0);
+//PID motorRight(0);
+// REALLY IMPORTANT TO DECREASE INPUT AND OUTPUT BUFFER SIZE, here set to 100 bytes each
+ros::NodeHandle_<ArduinoHardware, 6, 2, 2048, 2048> nh;
+
 
 // left motor pins on cytron
 #define dir_left_motor PA5 
@@ -36,36 +40,36 @@ PID yawPID(1);
 #define pinA_lifting PB13 
 #define pinB_lifting PB14
 
+#define pi 3.14159265359
 /*---------------- Variables declarations ----------------*/
 
 // counts for  encoders
 int count_left = 0, count_right = 0, count_lift = 0; 
 
 // Speeds for motors 
-float speed_left = 0, speed_right = 0;
-
+float speed_left = 0, speed_right = 0, RPS = 0;
+double rot_dist_left = 0, rot_dist_right = 0, R = 31, ang = 0;
 // IMU variables 
-float yaw_angle = 0;
+float yaw_angle = 0, desired_yaw = 0;
 
 // States for encoders 
-int current_state_l = 0, current_state_r = 0, current_state_lift = 0;
+int current_state_l = 0, current_state_r = 0, current_state_lift = 0, m_rot_desired = 40;
 
-// direction of encoders 
-String encdir_l ="", encdir_r ="", encdir_lift ="";  
 
 // Speeds variables and damping factor
-float y = 0, m = 0, epsi = 0.9;
+float y = 0, m = 0, m_rot = 0, epsi = 0.8, y_left = 0, y_right = 0;
 
 // PWMs for motors
 float pwm_left = 0.0, pwm_right = 0.0;
 
 // timers variables
-double prev_time = 0, speed_timer = 500;
+double prev_time = 0, speed_timer = 50;
 double action_timer = 10, last_action = 0;
 double feedback_timer = 1000, prev_feedback_timer = 0;
-
+double distance_right = 0.0, distance_left = 0.0, prev_distance = 0.0;
+int qr_separation = 50;
 // PID constants
-float kp = 0, ki = 0, kd = 0;
+float kp = 4, ki = 2, kd = 0.1;
 
 // Speeds feedback
 short int speeds_temp[2] = {0, 0};
@@ -75,7 +79,9 @@ short int speeds_temp[2] = {0, 0};
 std_msgs::Int16MultiArray feedback;
 ros::Publisher pub_feedback("feedback", &feedback);
 
-
+// Publisher for callback from stm
+std_msgs::Int8 stm_callback;
+ros::Publisher pub_stm_callback("ros_robot_moved_feedback_stm_callback", &stm_callback);
 /*---------------- Interrubts functions ----------------*/
 
 void funcL(){
@@ -83,14 +89,15 @@ void funcL(){
     
    // If the inputDT state is different than the inputCLK state then 
    // the encoder is rotating clockwise
-   if (digitalRead(pinB_encoder_left) != current_state_l) { 
+   if (digitalRead(pinB_encoder_left) != current_state_l) 
+   { 
      count_left--;
-     //encdir_l ="CCW";
      
-   } else {
+   } 
+   else 
+   {
      // Encoder is rotating counterclockwise
      count_left++;
-     //encdir_l ="CW";
    }
 
 }
@@ -100,14 +107,15 @@ void funcLift(){
     
    // If the inputDT state is different than the inputCLK state then 
    // the encoder is rotating clockwise
-   if (digitalRead(pinB_encoder_lifting) != current_state_lift) { 
+   if (digitalRead(pinB_encoder_lifting) != current_state_lift) 
+   { 
      count_lift++;
-     encdir_lift ="CW";
-     
-   } else {
+   } 
+   else 
+   {
      // Encoder is rotating counterclockwise
      count_lift --;
-     encdir_lift ="CCW";
+     
    }
 
 }
@@ -117,14 +125,15 @@ void funcR(){
    
    // If the inputDT state is different than the inputCLK state then 
    // the encoder is rotating clockwise
-   if (digitalRead(pinB_encoder_right) != current_state_r) { 
+   if (digitalRead(pinB_encoder_right) != current_state_r) 
+   { 
      count_right--;
-     encdir_r ="CCW";
      
-   } else {
+   } 
+   else 
+   {
      // Encoder is rotating counterclockwise
      count_right++;
-     encdir_r ="CW";
    }
 
 }
@@ -134,55 +143,314 @@ void funcR(){
 void setSpeeds(const std_msgs::Int16MultiArray& speeds)
 {
   short int *got = speeds.data;
-  y = got[0];
+  y = (300*got[0])/100;
   m = got[1];
+//  if(abs(y) > 0)
+//  {
+//  motorLeft.set_setpoint(y);
+//  motorRight.set_setpoint(y);
+//  motorLeft.set_constants(kp, ki, kd);
+//  motorRight.set_constants(kp, ki,kd);
+//  }
+  if(abs(y) > 0)
+  {
+  yawPID.set_setpoint(yaw_angle);
+  yawPID.set_constants(kp, ki,kd);
+  }
   debugger(y, "Speed: ");
   debugger(m, "Moment: ");
 }
-
+void setSpeedRot(const std_msgs::Int16& speeds)
+{
+  short int got = speeds.data;
+  m_rot_desired = got;
+  debugger(m_rot_desired, "rotation speed: ");
+}
 void setYaw(const std_msgs::Float32& yaw)
 {
   yaw_angle = yaw.data;
+  //debugger(yaw_angle, "Yaw: ");
 
 }
 
-void setYawPid(const std_msgs::Float32MultiArray& constants)
+void setQr(const std_msgs::Float32& qr)
+{
+  qr_separation = qr.data;
+  debugger(qr_separation, "qr_separation: ");
+
+}
+
+void setSpeedsLR(const std_msgs::Int16MultiArray& speeds)
+{
+  short int *got = speeds.data;
+  y_left = (255*got[0])/100;
+  y_right = (255*got[1])/100;
+
+  debugger(y_left, "y_left: ");
+  debugger(y_right, "y_right: ");
+}
+void setPid(const std_msgs::Float32MultiArray& constants)
 {
   kp = constants.data[0];
   ki = constants.data[1];
-  ki = constants.data[2];
+  kd = constants.data[2];
+//  motorLeft.set_constants(kp, ki, kd);
+//  motorRight.set_constants(kp, ki,kd);
+  yawPID.set_constants(kp, ki,kd);
+  debugger(kp, "kP: ");
+  debugger(ki, "kI: ");
+  debugger(kd, "kD: ");
 }
-
-ros::Subscriber<std_msgs::Int16MultiArray> sub_speeds("speeds", &setSpeeds );
+ros::Subscriber<std_msgs::Int16MultiArray> sub_speeds("ros_robot_move_stm", &setSpeeds );
+ros::Subscriber<std_msgs::Int16> sub_rot_speeds("rotate_speed", &setSpeedRot );
 ros::Subscriber<std_msgs::Float32> sub_yaw("yaw", &setYaw );
-ros::Subscriber<std_msgs::Float32MultiArray> sub_yaw_pid("yaw_pid", &setYawPid );
+ros::Subscriber<std_msgs::Float32> sub_qr("qr_separation", &setQr );
+ros::Subscriber<std_msgs::Int16MultiArray> sub_speeds_l_r("speeds", &setSpeedsLR );
+ros::Subscriber<std_msgs::Float32MultiArray> sub_pid("pid", &setPid );
+/*---------------- Check for rotation function ----------------*/
 
 
-/*---------------- Applying speed calculations function ----------------*/
-
-
-void move_robot()
+void check_rotation()
 {
-  
-  // calculating PWM for motors   
-  pwm_left = pwm_left*epsi + (y+m)*(1 - epsi);
-  pwm_right = pwm_right*epsi + (y-m)*(1 - epsi);
-  if (pwm_left >= 0){
-      analogWrite(pwm_left_motor,abs(pwm_left));
-      digitalWrite(dir_left_motor, HIGH);
+  // Algorithm for rotation clockwise
+  if (m == -1)
+  {
+    desired_yaw = yaw_angle + 90;
+    if (desired_yaw > 180)
+    {
+      desired_yaw = desired_yaw - 360;
     }
-    else{
-      analogWrite(pwm_left_motor,abs(pwm_left));
-      digitalWrite(dir_left_motor, LOW);
+    y = 0;
+    m_rot = m_rot_desired;
+    count_left = 0;
+    count_right = 0;
+    yawPID.set_constants(0, 0,0);
+    debugger(desired_yaw, "desired yaw: ");
+    while(1)
+    {
+      
+      pwm_left = pwm_left*epsi + (y+m_rot)*(1 - epsi);
+      pwm_right = pwm_right*epsi + (y-m_rot)*(1 - epsi);
+      if (pwm_left >= 0)
+      {
+        analogWrite(pwm_left_motor,abs(pwm_left));
+        digitalWrite(dir_left_motor, HIGH);
+      }
+      else
+      {
+        analogWrite(pwm_left_motor,abs(pwm_left));
+        digitalWrite(dir_left_motor, LOW);
+      }
+      if (pwm_right >= 0)
+      {
+        analogWrite(pwm_right_motor,abs(pwm_right));
+        digitalWrite(dir_right_motor, HIGH);
+      }
+      else
+      {
+        analogWrite(pwm_right_motor,abs(pwm_right));
+        digitalWrite(dir_right_motor, LOW);
+      }
+      if (yaw_angle >= desired_yaw)
+      {
+        analogWrite(pwm_left_motor,0);
+        analogWrite(pwm_right_motor,0);
+        
+        yawPID.set_constants(kp, ki,kd);
+        yawPID.set_setpoint(desired_yaw);
+        
+        distance_right = 0.0;
+        distance_left = 0.0;
+        m = 0;
+        m_rot = 0;
+        y = 0;
+        
+        stm_callback.data = 1;
+        pub_stm_callback.publish(&stm_callback);
+        break;
+      }
+      nh.spinOnce();
     }
-    if (pwm_right >= 0){
-      analogWrite(pwm_right_motor,abs(pwm_right));
-      digitalWrite(dir_right_motor, HIGH);
+    count_left = 0;
+    count_right = 0;
+    
+    
+  }
+  // Algorithm for rotation counterclockwise
+  if (m == 1)
+  {
+    count_left = 0;
+    count_right = 0;
+    yawPID.set_constants(0, 0,0);
+    desired_yaw = yaw_angle - 90;
+    if (desired_yaw < -180)
+    {
+      desired_yaw = desired_yaw + 360;
     }
-    else{
-      analogWrite(pwm_right_motor,abs(pwm_right));
-      digitalWrite(dir_right_motor, LOW);
+    y = 0;
+    m_rot = -m_rot_desired;
+    debugger(desired_yaw, "desired yaw: ");
+    while(1)
+    {
+      
+      pwm_left = pwm_left*epsi + (y+m_rot)*(1 - epsi);
+      pwm_right = pwm_right*epsi + (y-m_rot)*(1 - epsi);
+      if (pwm_left >= 0)
+      {
+        analogWrite(pwm_left_motor,abs(pwm_left));
+        digitalWrite(dir_left_motor, HIGH);
+      }
+      else
+      {
+        analogWrite(pwm_left_motor,abs(pwm_left));
+        digitalWrite(dir_left_motor, LOW);
+      }
+      if (pwm_right >= 0)
+      {
+        analogWrite(pwm_right_motor,abs(pwm_right));
+        digitalWrite(dir_right_motor, HIGH);
+      }
+      else
+      {
+        analogWrite(pwm_right_motor,abs(pwm_right));
+        digitalWrite(dir_right_motor, LOW);
+      }
+      if (yaw_angle <= desired_yaw)
+      {
+        
+        analogWrite(pwm_left_motor,0);
+        analogWrite(pwm_right_motor,0);
+//        motorLeft.set_constants(0, 0,0);
+//        motorRight.set_constants(0, 0,0);
+        yawPID.set_constants(kp, ki,kd);
+        yawPID.set_setpoint(desired_yaw);
+        distance_right = 0.0;
+        distance_left = 0.0;
+        m = 0;
+        m_rot = 0;
+        y = 0;
+        
+        stm_callback.data = 1;
+        pub_stm_callback.publish(&stm_callback);
+        break;
+      }
+     nh.spinOnce(); 
     }
+    count_left = 0;
+    count_right = 0;
+  }
+//if (m==-1)
+//    {
+//        count_left = 0;
+//        count_right = 0;
+//        while(1)
+//        {
+//          y = 0;
+//          m_rot = 45;
+//          ang = abs(count_left*0.3733);
+//          pwm_left = pwm_left*epsi + (y+m_rot)*(1 - epsi);
+//          pwm_right = pwm_right*epsi + (y-m_rot)*(1 - epsi);
+//          if (pwm_left >= 0)
+//          {
+//            analogWrite(pwm_left_motor,abs(pwm_left));
+//            digitalWrite(dir_left_motor, HIGH);
+//          }
+//          else
+//          {
+//            analogWrite(pwm_left_motor,abs(pwm_left));
+//            digitalWrite(dir_left_motor, LOW);
+//          }
+//          if (pwm_right >= 0)
+//          {
+//            analogWrite(pwm_right_motor,abs(pwm_right));
+//            digitalWrite(dir_right_motor, HIGH);
+//          }
+//          else
+//          {
+//            analogWrite(pwm_right_motor,abs(pwm_right));
+//            digitalWrite(dir_right_motor, LOW);
+//          }
+//          if(ang >= 90)
+//          {
+//            analogWrite(pwm_left_motor,0);
+//            analogWrite(pwm_right_motor,0);
+//            motorLeft.set_constants(0, 0,0);
+//            motorRight.set_constants(0, 0,0);
+//            distance_right = 0.0;
+//            distance_left = 0.0;
+//            m = 0;
+//            m_rot = 0;
+//            y = 0;
+//            
+//            stm_callback.data = 1;
+//            pub_stm_callback.publish(&stm_callback);
+//            break;
+//          }
+//          nh.spinOnce();
+//        }
+//        count_left = 0;
+//        count_right = 0;
+//        debugger(ang, "Angle ");
+//        
+//    }
+//    if (m==1)
+//    {
+//
+//        count_left = 0;
+//        count_right = 0;
+// 
+//        while(1)
+//        {
+//
+//          y = 0;
+//          m_rot = -45;
+//          ang = abs(count_right*0.3733);
+//          pwm_left = pwm_left*epsi + (y+m_rot)*(1 - epsi);
+//          pwm_right = pwm_right*epsi + (y-m_rot)*(1 - epsi);
+//          if (pwm_left >= 0)
+//          {
+//            analogWrite(pwm_left_motor,abs(pwm_left));
+//            digitalWrite(dir_left_motor, HIGH);
+//          }
+//          else
+//          {
+//            analogWrite(pwm_left_motor,abs(pwm_left));
+//            digitalWrite(dir_left_motor, LOW);
+//          }
+//          if (pwm_right >= 0)
+//          {
+//            analogWrite(pwm_right_motor,abs(pwm_right));
+//            digitalWrite(dir_right_motor, HIGH);
+//          }
+//          else
+//          {
+//            analogWrite(pwm_right_motor,abs(pwm_right));
+//            digitalWrite(dir_right_motor, LOW);
+//          }
+//          if(ang >= 90)
+//          {
+//            analogWrite(pwm_left_motor,0);
+//            analogWrite(pwm_right_motor,0);
+//            motorLeft.set_constants(0, 0,0);
+//            motorRight.set_constants(0, 0,0);
+//            m = 0;
+//            m_rot = 0;
+//            y = 0;
+//            distance_right = 0.0;
+//            distance_left = 0.0;
+//            stm_callback.data = 1;
+//            pub_stm_callback.publish(&stm_callback);
+//            break;
+//          }
+//          nh.spinOnce();
+//        }
+//        count_left = 0;
+//        count_right = 0;
+//        debugger(ang, "Angle ");
+//        
+//    }
+//  
+
 }
 
 void debugger(float data, String topic)
@@ -209,76 +477,111 @@ void setup() {
   pinMode(pinB_encoder_lifting,INPUT);
   //delay(100);
   //ISR for encoders declarations
-  attachInterrupt(digitalPinToInterrupt(pinA_encoder_left), funcL,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(pinA_encoder_lifting), funcLift,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(pinA_encoder_right), funcR,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinA_encoder_left), funcL,RISING);
+  attachInterrupt(digitalPinToInterrupt(pinA_encoder_lifting), funcLift,RISING);
+  attachInterrupt(digitalPinToInterrupt(pinA_encoder_right), funcR,RISING);
   
 
   // Ros nodes declarations
   nh.initNode();
   nh.subscribe(sub_speeds);
   nh.subscribe(sub_yaw);
-  nh.subscribe(sub_yaw_pid);
+  nh.subscribe(sub_qr);
+  nh.subscribe(sub_speeds_l_r);
+  nh.subscribe(sub_rot_speeds);
+  nh.subscribe(sub_pid);
+
   nh.advertise(pub_feedback);
-  
+  nh.advertise(pub_stm_callback);
+
   // PID constants and constraints
-  yawPID.set_constants(kp, ki,kd);
-  yawPID.set_constrains(-255, 255);
-  yawPID.set_setpoint(yaw_angle);
-  
+//  motorLeft.set_constants(kp, ki,kd);
+//  motorLeft.set_constrains(-255, 255);
+//  motorRight.set_constants(kp, ki,kd);
+//  motorRight.set_constrains(-255, 255);
+//  motorLeft.set_setpoint(y);
+//  motorRight.set_setpoint(y);
+    yawPID.set_constants(0,0,0);
+    yawPID.set_constrains(-255, 255);
+    yawPID.set_setpoint(yaw_angle);
 }
 
 void loop() {
   // Calculating linear speeds for motors
-  if ((millis()-prev_time) > speed_timer){
-    speed_left = (count_left/(450*(speed_timer/1000)))*60;
+  if ((millis()-prev_time) > speed_timer)
+  {
+    
+    distance_left = distance_left + abs(count_left)*0.0955;
+    distance_right = distance_right + abs(count_right)*0.0955;
+    speed_left = ((count_left/(230*(speed_timer/1000))))*60;
     count_left = 0;
 
-    speed_right = (count_right/(450*(speed_timer/1000)))*60;
+    speed_right = ((count_right/(230*(speed_timer/1000))))*60;
     count_right = 0;
+    // Sending Speeds to be applied
+//    y_right = motorRight.calc(speed_right);
+//    y_left = motorLeft.calc(speed_left);
+    
+
+    if ((distance_left >= qr_separation) && (m == 0))
+    {
+      if((distance_right >= qr_separation))
+      {
+      y = 0;
+      m_rot = 0;
+      analogWrite(pwm_left_motor,0);
+      analogWrite(pwm_right_motor,0);
+//      motorLeft.set_constants(0, 0,0);
+//      motorRight.set_constants(0, 0,0);
+      yawPID.set_constants(0, 0,0);
+      debugger(distance_left,"distance Left: ");
+      debugger(distance_right,"distance Right: ");
+      distance_left = 0;
+      distance_right = 0;
+      stm_callback.data = 1;
+      pub_stm_callback.publish(&stm_callback);
+      }
+    }
+    
     prev_time = millis();
     // Send speeds to feedback
-    speeds_temp[0] = round(speed_left);
+    speeds_temp[0] = round(speed_left);   
     speeds_temp[1] = round(speed_right);
-    Serial.print("Speed left: ");
-    Serial.println(speed_left);
-    Serial.print("Speed right: ");
-    Serial.println(speed_right);
-   
   }
-
-  
-  if ((millis() - last_action) > action_timer){
-    //move_robot();
+  check_rotation();
+  if ((millis() - last_action) > action_timer)
+  {
+    
+    m_rot = yawPID.calc(yaw_angle);
+    
     // Sending Speeds to be applied
-    pwm_left = pwm_left*epsi + (y+m)*(1 - epsi);
-    pwm_right = pwm_right*epsi + (y-m)*(1 - epsi);
-//    apply_speeds(pwm_left_motor, dir_left_motor, pwm_left);
-//    apply_speeds(pwm_right_motor, dir_right_motor, pwm_right);  
-if (pwm_left >= 0){
+    pwm_left = pwm_left*epsi + (y+m_rot)*(1 - epsi);
+    pwm_right = pwm_right*epsi + (y-m_rot)*(1 - epsi);
+    if (pwm_left >= 0)
+    {
       analogWrite(pwm_left_motor,abs(pwm_left));
       digitalWrite(dir_left_motor, HIGH);
     }
-    else{
+    else
+    {
       analogWrite(pwm_left_motor,abs(pwm_left));
       digitalWrite(dir_left_motor, LOW);
     }
-    if (pwm_right >= 0){
+    if (pwm_right >= 0)
+    {
       analogWrite(pwm_right_motor,abs(pwm_right));
       digitalWrite(dir_right_motor, HIGH);
     }
-    else{
+    else
+    {
       analogWrite(pwm_right_motor,abs(pwm_right));
       digitalWrite(dir_right_motor, LOW);
     }
 
-    
-    
-
-    last_action = millis();
+        last_action = millis();
     
   }
-
+  
   // Sending feedback values of motors using ROS
   if ((millis() - prev_feedback_timer) > feedback_timer)
   {
