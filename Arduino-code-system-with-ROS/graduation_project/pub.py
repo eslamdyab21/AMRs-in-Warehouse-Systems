d376952a1a9b7983dfac6f439b3ebde3c232b7c5
Islam Ashraf <eslamashraf20003@gmail.com>
2023-07-01 09:17:20 +0200

    Python file for testing movement on the pi - fixes #17

--- Arduino-code-system-with-ROS/graduation_project/pub.py
+++ Arduino-code-system-with-ROS/graduation_project/pub.py
@@ -5,16 +5,21 @@ import time
 from curtsies import Input
 from std_msgs.msg import Float64MultiArray, Float32MultiArray
 from std_msgs.msg import Float32MultiArray
-
+import os
 from std_msgs.msg import Int16MultiArray
 from std_msgs.msg import Float32
 from std_msgs.msg import Int16
 from std_msgs.msg import Byte
 
-
+local_ip='192.168.1.8' 
+roscore_ip='192.168.1.20'
+roscore_port = 11311
+os.environ['ROS_IP'] = local_ip
+os.environ['ROS_HOSTNAME'] = local_ip
+os.environ['ROS_MASTER_URI'] = 'http://' + roscore_ip + ':' + str(roscore_port)
 def talker():
     # pub_yaw = rospy.Publisher('yaw', Float32, queue_size=10)
-    pub_speeds = rospy.Publisher('speeds', Int16MultiArray, queue_size=10)
+    pub_speeds = rospy.Publisher('ros_robot_move_stm', Int16MultiArray, queue_size=10)
 
     rospy.init_node('nh', anonymous=True)
     rate = rospy.Rate(0.2) # 10hz
@@ -30,16 +35,20 @@ def talker():
                     y = 0
                     yaw = 0
                 elif e == 'm':
-                    y = int(input("horizental limit = "))
-                    yaw = int(input("vertical limit = "))
+                    y = int(input("y = "))
+                    yaw = int(input("m = "))
             
                 if e == 'w':
-                    y = 100
+                    y = 33
+                    yaw = 0
                 elif e == 's':
-                    y = -100
+                    y = -33
+                    yaw = 0
                 if e == 'd':
+                    y = 0
                     yaw = 100
                 elif e == 'a':
+                    y = 0
                     yaw = -100
 
                 
